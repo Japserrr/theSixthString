ec49240702bf3d2ecafa6c94b6884e98aa6c92a9
Timo van Rooijen <timovanrooijen@outlook.com>
2022-12-05 22:18:56 +0100

    Basic file structure + basic mvc structure

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+//site name
+define('SITE_NAME', 'The sixth string');
+
+//App Root
+define('APP_ROOT', dirname(dirname(__FILE__)));
+define('URL_ROOT', '/theSixthString');
+define('URL_SUBFOLDER', '');
+
+//DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'the_sixth_string');
