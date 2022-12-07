4b4c27c1499d94d2e8fb44faf9b9da694ba1771e
Japserrr <jaspergraber13@gmail.com>
2022-12-07 14:53:13 +0100

    Pagina toevoegen aan MVC en wat files refactoren

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -1,7 +1,11 @@
 <?php
 
-
 function home()
 {
     include_once '../views/home.php';
+}
+
+function productManagement(): void
+{
+    require_once('../views/product-management.html');
 }
--- views/home.php
+++ views/home.php
@@ -1,8 +1,3 @@
 <body>
     <h1 class="text-center"><?= SITE_NAME ?></h1>
-
-    <script src="../public/js/popper/popper.min.js"></script>
-    <script src="../public/js/bootstrap/bootstrap.min.js" ></script>
 </body>
-
-</html>
