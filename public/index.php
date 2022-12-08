fc86c05ecd444f8cf50859b77d635472c5b12b88
Timo van Rooijen <timovanrooijen@outlook.com>
2022-12-08 18:32:29 +0100

    Fixed issue with loading css and js files

--- public/index.php
+++ public/index.php
@@ -5,9 +5,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="./js/popper/popper.min.js"></script>
-    <script src="./js/bootstrap/bootstrap.min.js" ></script>
+    <link href="./public/css/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <script src="./public/js/popper/popper.min.js"></script>
+    <script src="./public/js/bootstrap/bootstrap.min.js"></script>
 
     <title><?= SITE_NAME ?></title>
 </head>
