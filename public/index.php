311ea255cea923f358e8b5cdcc6be2f2f7cef396
Timo van Rooijen <timovanrooijen@outlook.com>
2022-12-08 18:33:29 +0100

    Updated styles.css file

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="./public/css/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link href="./public/css/style.css" rel="stylesheet">
     <script src="./public/js/popper/popper.min.js"></script>
     <script src="./public/js/bootstrap/bootstrap.min.js"></script>
 
