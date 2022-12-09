9dcf7a1188158b5909e1536a9a22e52de969ea87
Dinny van Huizen <dinnyvanh@hotmail.com>
2022-12-09 17:08:46 +0100

    added more small changes

--- controllers/login/login_handler.js
+++ controllers/login/login_handler.js
@@ -1,32 +1,43 @@
+window.addEventListener('load', function () {
+
+
 // Create a function to handle the account creation
 function createAccount() {
-    // Get the values of the input fields
-    var name = document.getElementById("form_name");
-    var password = document.getElementById("form_password");
-    var confirmPassword = document.getElementById("form_control_password");
-    var regex = /'/;
-    // Check the username and password for single quotes
-    if (regex.test(name) || regex.test(password) || regex.test(confirmPassword)) {
-      alert("Invalid input: single quotes are not allowed");
-      return;
-    }
-    alert("ok")
-    var parsedPassword = password.replace(/[!@#$%^]/g, "\\$&");
-    var confirmedParsedPassword = confirmPassword.replace(/[!@#$%^]/g, "\\$&");
-    // Validate the input values
-    if (name == "") {
-      alert("Please enter a username");
-      return;
-    }
-    if (parsedPassword == "") {
-      alert("Please enter a password");
-      return;
-    }
-    if (parsedPassword != confirmedParsedPassword) {
-      alert("The passwords do not match");
-      return;
-    }
+  // Get the values of the input fields
+  let name = document.getElementById("form_name").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("form_password").value;
+  let confirmPassword = document.getElementById("form_control_password").value;
+  console.log(name);
+  //check with regex for character ' 
+  let regex = new RegExp(/'/g) ;
   
-    // Create the account
-    alert("Account created successfully!");
+  // Check if any of the fields are empty
+
+  if(name.value == "" || password.value == "" || confirmPassword.value == "") {
+    alert("Please fill out all fields");
+    return;
   }
+  console.log( regex.test(name))
+
+  // Check the username and password for single quotes
+  if (regex.test(name) || regex.test(password) || regex.test(confirmPassword) || regex.test(email)) {
+    alert("Invalid input: single quotes are not allowed");
+    return;
+  }
+  
+  // let parsedPassword = password.replace(/[!@#$%^]/g, "\\$&");
+  // let confirmedParsedPassword = confirmPassword.replace(/[!@#$%^]/g, "\\$&");
+  
+
+  // if (parsedPassword != confirmedParsedPassword) {
+  //   alert("The passwords do not match");
+  //   return;
+  // }
+ 
+  // Create the account
+  alert("Account created successfully!");
+}
+
+});
+
