4c3a818094e2072ae951d583fd7353c2301e7c4f
Dinny van Huizen <dinnyvanh@hotmail.com>
2022-12-11 17:31:58 +0100

    little more work

--- controllers/login/login_handler.js
+++ controllers/login/login_handler.js
@@ -1,33 +1,43 @@
-window.addEventListener('load', function () {
-
 
 // Create a function to handle the account creation
 function createAccount() {
-  // Get the values of the input fields
-  let name = document.getElementById("form_name").value;
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("form_password").value;
-  let confirmPassword = document.getElementById("form_control_password").value;
-  console.log(name);
-  //check with regex for character ' 
-  let regex = new RegExp(/'/g) ;
-  
-  // Check if any of the fields are empty
 
-  if(name.value == "" || password.value == "" || confirmPassword.value == "") {
-    alert("Please fill out all fields");
-    return;
-  }
-  console.log( regex.test(name))
 
-  // Check the username and password for single quotes
-  if (regex.test(name) || regex.test(password) || regex.test(confirmPassword) || regex.test(email)) {
-    alert("Invalid input: single quotes are not allowed");
-    return;
-  }
+  var accountDetails = [];
+  // Get the values from the form
+  for (const element of document.forms[0].elements) {
   
-  // let parsedPassword = password.replace(/[!@#$%^]/g, "\\$&");
-  // let confirmedParsedPassword = confirmPassword.replace(/[!@#$%^]/g, "\\$&");
+    if(element.nodeName != "INPUT")
+    {
+      continue;
+    }
+    if(element.value == "") {
+      
+      alert("Please fill out all fields");
+      return;
+    }
+    if(checkRegex(element.value))
+    {
+      alert("Invalid input: single quotes are not allowed");
+
+      return;
+    }
+
+    accountDetails.push({name: element.id, value: element.value});
+  }
+ 
+
+
+  let parsedPassword = password.replace(/[!@#$%^]/g, "\\$&");
+  let confirmedParsedPassword = confirmPassword.replace(/[!@#$%^]/g, "\\$&");
+
+  alert("Account created successfully!");
+
+ 
+
+  
+  
+  
   
 
   // if (parsedPassword != confirmedParsedPassword) {
@@ -36,8 +46,14 @@ function createAccount() {
   // }
  
   // Create the account
-  alert("Account created successfully!");
 }
+function checkRegex(value)
+{
+  var regex = new RegExp(/'/g) ;
 
-});
-
+  if(regex.test(value))
+  {
+    return true;
+  }
+ return false;
+}
