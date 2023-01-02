129d5425a40077f55ea16dcbe4c1d8bc138248b2
Dinny van Huizen <dinnyvanh@hotmail.com>
2023-01-02 20:06:38 +0100

    removed ;

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -5,7 +5,7 @@ function convertionRatio(): void
 
     $registered_users = registeredusers($conn);
     $total_orders = totalorders($conn);
-    $ratio = ($registered_users)["user_count"] > 1 ? 100/intval(($registered_users)["user_count"])*intval(($total_orders)["order_count"]) : 0;
+    $ratio = ($registered_users)["user_count"] > 1 ? 100 / intval(($registered_users)["user_count"]) * intval(($total_orders)["order_count"]) : 0;
     $conn = null;
     require_once('../views/statistics.phtml');
 }
@@ -14,11 +14,10 @@ function registeredusers($conn)
 {
     $prepared = $conn->prepare("SELECT COUNT(*) as user_count, MAX(created_at) as latest_created_at FROM auth");
     $prepared->execute();
-if($prepared->rowCount()>0)
-{
-    return $prepared->fetch(PDO::FETCH_ASSOC);
-}
-return 0;
+    if ($prepared->rowCount() > 0) {
+        return $prepared->fetch(PDO::FETCH_ASSOC);
+    }
+    return 0;
 }
 
 function totalorders($conn)
@@ -27,11 +26,9 @@ function totalorders($conn)
     $result1 = $conn->prepare("SELECT COUNT(*) as order_count, MAX(order_date) as last_order FROM `order`");
     $result1->execute();
 
-    if($result1->rowCount()>0)
-    {
+    if ($result1->rowCount() > 0) {
 
         return $result1->fetch(PDO::FETCH_ASSOC);
     }
     return 0;
 }
-?>;
--- controllers/AdminPagecontroller.php
+++ controllers/AdminPagecontroller.php
@@ -11,21 +11,22 @@ function AdminPage()
 function SelectEmployee()
 {
     $conn = getDbConnection();
-   
+
     $r = $conn->prepare("SELECT auth_id FROM employee WHERE auth_id = ?");
     $r->execute();
     $product = $r->fetchAll();
     $conn = null;
     return $product;
-
 }
 
 function UpdateEmployee()
 {
-    
+
     $conn = getDbConnection();
     $r = $conn->prepare("UPDATE employee SET auth_id WHERE ? ", [PDO::ATTR_CURSOR, PDO::CURSOR_FWDONLY]);
 
-    $r->execute([/**hier komt je auth_id variable */]);
-return $r;
+    $r->execute([
+        /**hier komt je auth_id variable */
+    ]);
+    return $r;
 }
