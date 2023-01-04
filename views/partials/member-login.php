c96e41d5f28f16ec568ab479ad5db5515c0767b0
hussain-psdk <psdkha@hotmail.com>
2023-01-04 03:25:36 -0700

    Update member-login.php
    update member login form

--- views/partials/member-login.php
+++ views/partials/member-login.php
@@ -34,9 +34,7 @@ $users = $stmt->fetchAll(PDO::FETCH_OBJ);
 <?php
 // update 2
 $auth_id    = 1;
-$sql        = 'select * from `auth` where id = :auth_id';
-//$sql        = 'select * from address where id = :id';
-//$sql        = 'select * from auth where id = :id';
+$sql        = 'select * from `auth` where id = : ';
 $stmt       =   $conn->prepare($sql);
 
 $stmt->execute(
@@ -46,74 +44,35 @@ $stmt->execute(
 );
 $user       =   $stmt->fetch(PDO::FETCH_ASSOC);
 
-if (isset($_POST['first_name']) && isset($_POST['last_name'])) {
-    $auth_id        = $user['auth_id'];
-    $first_name     = $_POST['first_name'];
-    $infix          = $_POST['infix'];
-    $last_name      = $_POST['last_name'];
-    $phone_number      = $_POST['phone_number'];
-    $street_name    = $_POST['street_name'];
-    $zipcode        = $_POST['zipcode'];
-    $house_number   = $_POST['house_number'];
-    $city           = $_POST['city'];
-    $country        = $_POST['country'];
-    $password       = $_POST['password'];
-    $email          = $_POST['email'];
+if (isset($_POST)) {
+    if (!empty($_POST['password'])
+        && !empty($_POST['email'])
+    ) {
+        $password = $_POST['password'];
+        $email = $_POST['email'];
 
+        $params = [
+            'password' => $password,
+            'email' => $email
+        ];
+        $login = '';
 
-    $user_table = 'UPDATE `user` set first_name = :first_name, last_name = :last_name, infix = :infix, phone_number = :phone_number where auth_id = :auth_id';
-    $address_table = 'UPDATE `address` set street_name = :street_name, zipcode = :zipcode, house_number = :house_number, city = :city, country = :country  where id = :id';
-    $auth_table = 'UPDATE `auth` set password = :password, email = :email where id = :id';
-
-    $statement_user = $conn->prepare($user_table);
-    $statement_table = $conn->prepare($address_table);
-    $statement_auth = $conn->prepare($auth_table);
-    if
-    (
-        $statement_user->execute
-        (
-            [
-                'auth_id'       => $auth_id,
-                'first_name'    => $first_name,
-                'infix'         => $infix,
-                'last_name'     => $last_name,
-                'phone_number'     => $phone_number
-//                    'street_name'   => $street_name,
-//                    'zipcode'       => $zipcode,
-//                    'house_number'  =>$house_number,
-//                    'city'          => $city,
-//                    'country'       => $country,
-//                    'password'      => $password,
-//                    'email'         => $email
-
-            ]
-        )
-    )if
-    (
-        $statement_table->execute(
-            (
-            [
-                'street_name'   => $street_name,
-                'zipcode'       => $zipcode,
-                'house_number'  =>$house_number,
-                'city'          => $city,
-                'country'       => $country
-            ]
-            )
-        )
-    )if (
-            $statement_auth->execute(
-                (
-                [
-                    'password'      => $password,
-                    'email'         => $email
-                ]
-                )
-            )
-        )
-        {
-            header("Location: edit-info");
+        if (empty($user)) {
+            $login = '
+        INSERT INTO `auth`
+        VALUES (null, :password, :email)';
+        } else {
+            $params['loginid'] = $user;
+            $login = '
+        UPDATE `auth`
+        SET password    =   :password,
+            email       =    :email
+            WHERE id    = :id';
         }
+        $statement_auth = $conn->prepare($login);
+        $statement_auth->execute($params);
+        header("Location: edit-login");
+    }
 }
 ?>
 <div class="container rounded bg-white mt-5 mb-5">
@@ -123,7 +82,7 @@ if (isset($_POST['first_name']) && isset($_POST['last_name'])) {
                 <div class="d-flex justify-content-between align-items-center mb-3">
                     <h4 class="text-right">inlog gegevens</h4>
                 </div>
-                <form action="edit-info" method="POST">
+                <form action="edit-login" method="POST">
                     <div class="row mt-2">
                         <div class="col-md-6"><label class="labels">Email adres</label><input type="email" class="form-control" name="email" placeholder="Email adres" value=""></div>
                         <div class="col-md-6"><label class="labels">Password</label><input type="password" class="form-control" name="password" placeholder="password" value=""></div>
