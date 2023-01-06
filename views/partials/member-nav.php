e22d217ce39d0a592e39fcd85a34a355b518757a
hussain-psdk <psdkha@hotmail.com>
2023-01-06 09:05:18 -0500

    Update member-nav.php

--- views/partials/member-nav.php
+++ views/partials/member-nav.php
@@ -6,7 +6,6 @@ if (isLoggedIn() && !check_expire_time()) {
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="/theSixthString">Home</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
