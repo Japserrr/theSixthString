7f33ff244eaa09057576311eeaea90347ba4ad6c
hussain-psdk <psdkha@hotmail.com>
2023-01-06 09:01:25 -0500

    update profile navbar

--- views/partials/member-nav.php
+++ views/partials/member-nav.php
@@ -1,3 +1,8 @@
+<?php
+if (isLoggedIn() && !check_expire_time()) {
+
+
+?>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -19,3 +24,5 @@
             </li>
 
 </nav>
+
+<?php }
