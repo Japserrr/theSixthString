0b827a363766535301af2653836c2fb848930bd5
Dinny van Huizen <dinnyvanh@hotmail.com>
2023-01-07 12:22:57 +0100

    added db code to config

--- config/config.php
+++ config/config.php
@@ -4,24 +4,6 @@
 
 ///GEBRUIK DIT VOOR OP DE PERSOONLIJKE PC
 //site name
-define('SITE_NAME', 'The Sixth String');
-
-//App Root
-define('APP_ROOT', dirname(dirname(__FILE__)));
-define('URL_MAIN', '');
-define('URL_ROOT', '');
-define('URL_SUBFOLDER', '');
-
-//DB Params
-define('DB_HOST', 'localhost');
-define('DB_PORT', '3306');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'the_sixth_string');
-
-
-/// GEBRUIK DIT VOOR OP DE WEBSERVER
-// //site name
 // define('SITE_NAME', 'The Sixth String');
 
 // //App Root
@@ -31,8 +13,26 @@ define('DB_NAME', 'the_sixth_string');
 // define('URL_SUBFOLDER', '');
 
 // //DB Params
-// define('DB_HOST', '172.16.0.12');
-// define('DB_PORT', '4269');
-// define('DB_USER', 'webserver');
-// define('DB_PASS', 'Windesheim#2');
+// define('DB_HOST', 'localhost');
+// define('DB_PORT', '3306');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
 // define('DB_NAME', 'the_sixth_string');
+
+
+/// GEBRUIK DIT VOOR OP DE WEBSERVER
+// //site name
+define('SITE_NAME', 'The Sixth String');
+
+//App Root
+define('APP_ROOT', dirname(dirname(__FILE__)));
+define('URL_MAIN', '');
+define('URL_ROOT', '');
+define('URL_SUBFOLDER', '');
+
+//DB Params
+define('DB_HOST', '172.16.0.12');
+define('DB_PORT', '4269');
+define('DB_USER', 'webserver');
+define('DB_PASS', 'Windesheim#2');
+define('DB_NAME', 'the_sixth_string');
