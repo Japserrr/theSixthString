dfa201a8fe8912c884d4349bed104dca47d2aedd
Nbruyn <117454443+Nbruyn@users.noreply.github.com>
2023-01-07 15:34:10 +0100

    add comments to code

--- controllers/AdminPagecontroller.php
+++ controllers/AdminPagecontroller.php
@@ -1,33 +1,42 @@
 <?php
 
 include_once '../helpers/database.php';
-
+//load admin.phtml  so that employees can be used.
 function adminPage()
-{
+{   
+   
     $employees = getAllEmployee();
     require_once('../views/admin.phtml');
 }
-
+//get all employees
 function getAllEmployee()
 {
+    //get db connection
     $conn = getDbConnection();
+    //prepare SQL  Query
     $r = $conn->prepare("
     SELECT u.*, 
     a.email
     FROM user AS u
     INNER JOIN auth AS a ON a.id = u.auth_id  
     WHERE u.employee = 1");
+    // Excute Query
     $r->execute();
+    // Get result from Query
     $users = $r->fetchAll();
+    //Close connection
     $conn = null;
+    //Return Users
     return $users;
 }
-
+//select employee
 function selectEmployee()
 {
+    //fetches email from POST with array key searchfield
     $email = $_POST['searchField'];
-
+//get db connection
     $conn = getDbConnection();
+    //prepare SQL  Query
     $r = $conn->prepare("
         SELECT 
         u.*,
@@ -36,23 +45,29 @@ function selectEmployee()
         INNER JOIN auth AS a ON a.id = u.auth_id 
         WHERE a.email LIKE ?
     ");
+    // Execute Query
     $r->execute(["%$email%"]);
+    // Get result from Query
     $employees = $r->fetchAll();
+     //Close connection
     $conn = null;
-
+    //load admin.phtml
     require_once('../views/admin.phtml');
 }
 
 function updateEmployee()
 {
-    // validation toevoegenb
+    //fetches ID from POST with array key ID
     $id = $_POST['id'];
+    //  Converts the value of employee to an integer.
     $employee = intval($_POST['employee']);
+    //get db connection
     $conn = getDbConnection();
-
+//prepare and executes SQL  Query
     $r = $conn->prepare("UPDATE user SET employee = :employee WHERE auth_id = :id")->execute(['employee' => $employee, 'id' => $id]);
-
+//Close connection
     $conn = null;
+    // redirect to /medewerkers
     header('Location: ' . URL_MAIN . URL_ROOT . '/medewerkers');
     exit;
 }
--- controllers/PageController.php
+++ controllers/PageController.php
@@ -1,20 +1,29 @@
 <?php
 
 include_once '../helpers/database.php';
-
+// if the function doenst return a value
 function homepage(): void
 {
     $values = [];
+    //get db connection
     $conn = getDbConnection();
+
     $sql = "SELECT id,product_name,img_path FROM product";
+    //check if data is enter into the searchfield.
     if (isset($_GET["search"])) {
+        // if there is a value in search field  add the WHERE clause to the query
         $sql .= " WHERE `product_name` LIKE :search";
+        // check if the value contains  the criteria that was enter into the searchfield
         $values = ['search' => '%' . $_GET['search'] . '%'];
     }
+    //prepare SQL  Query
     $r = $conn->prepare($sql);
+    // Execute Query
     $r->execute($values);
+     // Get result from Query
     $products = $r->fetchAll();
+     //Close connection
     $conn = null;
-
+//load productlist.phtml
     require_once('../views/productlist.phtml');
 }
