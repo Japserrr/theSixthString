798803ee06c96b5f724c5d1316b9283ed00b5c30
hussain-psdk <psdkha@hotmail.com>
2023-01-23 21:10:44 +0100

    live versie

--- config/config.php
+++ config/config.php
@@ -7,31 +7,31 @@ define('SITE_NAME', 'The Sixth String');
 
 
 //App Root
- define('APP_ROOT', dirname(dirname(__FILE__)));
- define('URL_MAIN', 'http://localhost');
- define('URL_ROOT', '/theSixthString');
- define('URL_SUBFOLDER', '');
+// define('APP_ROOT', dirname(dirname(__FILE__)));
+// define('URL_MAIN', 'http://localhost');
+// define('URL_ROOT', '/theSixthString');
+// define('URL_SUBFOLDER', '');
 
 // //DB Params
- define('DB_HOST', 'localhost');
- define('DB_PORT', '3306');
- define('DB_USER', 'root');
- define('DB_PASS', '');
- define('DB_NAME', 'the_sixth_string');
+// define('DB_HOST', 'localhost');
+// define('DB_PORT', '3306');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'the_sixth_string');
 
 
 /// GEBRUIK DIT VOOR OP DE WEBSERVER
-// //site name
+ //site name
 
-// //App Root
-//define('APP_ROOT', dirname(dirname(__FILE__)));
-//define('URL_MAIN', '');
-//define('URL_ROOT', '');
-//define('URL_SUBFOLDER', '');
+ //App Root
+define('APP_ROOT', dirname(dirname(__FILE__)));
+define('URL_MAIN', '');
+define('URL_ROOT', '');
+define('URL_SUBFOLDER', '');
 
 //DB Params
-//define('DB_HOST', '172.16.0.12');
-//define('DB_PORT', '4269');
-//define('DB_USER', 'webserver');
-//define('DB_PASS', 'Windesheim#2');
-//define('DB_NAME', 'the_sixth_string');
+define('DB_HOST', '172.16.0.12');
+define('DB_PORT', '4269');
+define('DB_USER', 'webserver');
+define('DB_PASS', 'Windesheim#2');
+define('DB_NAME', 'the_sixth_string');
